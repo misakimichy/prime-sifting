59f7bd369d91f2f47f0a81e2f565d1dec929ac8f
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-30 17:38:40 -0700

    Clean up the code.

--- Models/PrimeSifting.cs
+++ Models/PrimeSifting.cs
@@ -6,13 +6,6 @@ namespace PrimeSifting.Models
   {
     public static List<int> GetPrimes(int number)
     {
-      // create a list of integers based on the inputNumber
-      // List<int> numbers = new List<int>();
-      // for (int i = 2; i <= number; i++)
-      // {
-      //   numbers.Add(i);
-      // }
-
       // loop through each numbers in the list, adding each prime number to Primes
       List<int> primes = new List<int>();
       bool isPrime = true;
